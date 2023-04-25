f1eb320cb39c6d6aac798913a5323aa0a20cbc21
Peakprakash <prakashkbasnet01@gmail.com>
2023-04-25 04:46:36 -0700

    createtodo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Post } from '@nestjs/common/decorators';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private TodoService: TodoService) {}
+  @Post()
+  createtodo() {
+    return this.TodoService.createtodo();
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private prisma: PrismaService) {}
+  async createtodo() {
+    const todo = await this.prisma.todo.create({
+      data: { tittle: 'learn nestjs' },
+    });
+    console.log(todo);
+    return todo;
+  }
+}
